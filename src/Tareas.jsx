a36816466a528babe761b297c906b485da4c3c2b
Andrea Lopez <skarletswdev@gmail.com>
2023-11-01 15:47:38 -0300

    preventDefault + useRef

--- src/Tareas.jsx
+++ src/Tareas.jsx
@@ -1,8 +1,26 @@
+import { useRef } from "react";
+
 function Tareas() {
+
+    const inputRef = useRef();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        console.log(inputRef.current.value);
+    }
 
     return (
       <>
         <h1>Tareas</h1>
+
+        <form onSubmit={handleSubmit}>
+
+            <label htmlFor="title">Titulo</label>
+            <input type="text" id="title" ref={inputRef}/>
+            <button type="submit">Enviar</button>
+
+        </form>
       </>
     )
   }
